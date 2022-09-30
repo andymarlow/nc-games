45994d1f5261a4a9f3b9b33371d1f19cdb240996
andymarlow <andymarlow4@gmail.com>
2022-09-30 16:05:14 +0100

    Voting feature, instant display response, err hand

---
+++ src/components/ReviewVote.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { patchReviewVote } from "../utils/api";
+
+const ReviewVote = ({ review_id, votes }) => {
+  const [voteChange, setVoteChange] = useState(0);
+  const [voteErr, setVoteErr] = useState(null);
+  const visableVotes = votes + voteChange;
+
+  const voteFunction = (singleVote) => {
+    switch (singleVote) {
+      case -1:
+      case 0:
+      case 1:
+        patchReviewVote(review_id, singleVote)
+          .then(() => {
+            setVoteErr(null);
+          })
+          .catch(() => {
+            setVoteErr(true);
+            setVoteChange(voteChange);
+          });
+        setVoteChange((value) => value + singleVote);
+        break;
+      default:
+        setVoteErr(null);
+    }
+  };
+
+  return (
+    <div>
+      <p>Votes {visableVotes} </p>
+      <button onClick={() => voteFunction(1)}>Up Vote</button>
+      <button onClick={() => voteFunction(-1)}>Down Vote</button>
+      {voteErr ? <p>Vote failed, please vote again</p> : ""}
+    </div>
+  );
+};
+export default ReviewVote;
